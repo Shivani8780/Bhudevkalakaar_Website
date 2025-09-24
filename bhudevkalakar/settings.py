fedd87a812c896acdab99f25eebd93ee9fa7b9a2
Shivani Mishra <mishrashivani8780@gmail.com>
2025-09-24 12:42:23 +0530

    change commit

--- bhudevkalakar/settings.py
+++ bhudevkalakar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rn#t@=jp5%4up41-v8h*-mmg81*m+kb06$zv9!qq^@x^#5zc^y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '79977cbb1ead.ngrok-free.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '43bb82ae641a.ngrok-free.app']
 
 
 # Application definition
@@ -130,5 +130,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://79977cbb1ead.ngrok-free.app'
+    'https://43bb82ae641a.ngrok-free.app'
 ]
